eae8962b4f5906b4da705f2fa481ffe7433ff7a3
gbhutra <mail.gbhutra@gmail.com>
2016-12-13 16:45:13 -0600

    giving permission to public folder

--- app/index.js
+++ app/index.js
@@ -18,8 +18,42 @@ wagner.invoke(require('./api/authentication'), { app: app, User: user });
 
 app.use('/',require('./api/asset_api')(wagner));
 
+app.use(express.static(path.join(__dirname, '../public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+
+
 var port = process.env.PORT || config.PORT;
 app.set('port', port);
 app.listen(port);
-
 console.log('Listening on port '+port);
